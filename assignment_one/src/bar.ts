52d7fbb121ae8b45f0445e1deb13b3bd47b5b1b1
BuildTools <unconfigured@null.spigotmc.org>
2023-04-28 17:24:32 +0200

    add refactored code

--- assignment_one/src/bar.ts
+++ assignment_one/src/bar.ts
@@ -1,35 +1,45 @@
-export let BEER_ONE = "heineken";
-export let BEERTWO = 'corona';
-export let CocktailOne = `mojito`;
-export let cocktail2="margerita"
-export let special_cocktail="special_bacardi";
+export let BEER_ONE : string = "heineken";
+export let BEER_TWO : string = "corona";
+export let COCKTAIL_ONE : string = "mojito";
+export let COCKTAIL_TWO : string = "margerita";
+export let COCKTAIL_SPECIAL : string = "special_bacardi";
 
-export function calculateCost(drink, isStudent, amount) {
-    if (amount> 2 && (drink == CocktailOne||drink == cocktail2)) {
-    throw new Error("Not allowed to order more than 2 cocktails");
-    }
-    var finalPrice;
-        if (drink == BEER_ONE) {
-            finalPrice = 74;
-        }
-    else if (drink == CocktailOne) {
-        finalPrice = 103;
-    }
-    else if (drink == BEERTWO) finalPrice = 110;
-    else if (drink == BEERTWO) finalPrice = 110;
-    else if (drink == cocktail2) {
-        finalPrice = ingredient_6() + ingredient_5() + ingredient_4();
-    }
-    else if (drink == special_cocktail) {finalPrice = ingredient_6()/2 
-        + ingredient_1() + ingredient_2() + ingredient_3();
-    }
-    else {
+const COCKTAILS : string[] = [ COCKTAIL_ONE, COCKTAIL_TWO, COCKTAIL_SPECIAL ];
+const BEERS : string[] = [ BEER_ONE, BEER_TWO ];
+
+export const isCocktail = (drink:string='') =>
+       COCKTAILS.indexOf(drink.toLowerCase())>-1;
+export const isBeer = (drink:string='') =>
+       BEERS.indexOf(drink.toLowerCase())>-1;
+
+// TODO: use Map
+// Memorize prices for drinks.
+const PRICES : object = {}
+
+export function calculateCost(drink:string='',
+       isStudent:boolean=false, amount:number=1) {
+    // convert user input to lower case
+    drink = drink.toLowerCase();
+
+    if ( !isBeer(drink) && !isCocktail(drink) ){
         throw new Error("Drink does not exist");
     }
-    if (isStudent && (drink ==BEER_ONE||drink == BEERTWO)) {
+
+    amount = Number.parseInt(amount)
+    if ( !Number.isFinite(amount) || amount<1 ) {
+       throw new Error("Invalid amount of drinks ordered");
+    }
+    
+    if (amount>2 && isCocktail(drink) ) {
+       throw new Error("Not allowed to order more than 2 cocktails");
+    }
+    
+    var finalPrice = PRICES[drink];
+    
+    if (isStudent && isBeer(drink)) {
         finalPrice = finalPrice-finalPrice/10;
     }
-    if (!finalPrice) return;
+    
     return Math.ceil(finalPrice*amount);
 }
 
@@ -53,12 +63,26 @@ function ingredient_4() {
     return 10;
 }
 
-/**
- * Tonic water
- */ 
+// Tonic water 
 const ingredient_5 = () => 20;
 
 // Gin
 function ingredient_6() {
     return 85;
 }
+
+// shake it well
+PRICES[COCKTAIL_TWO] =  ingredient_6() + ingredient_5() + ingredient_4();
+
+// my favorite
+PRICES[COCKTAIL_SPECIAL] = ingredient_6()/2 
+  + ingredient_1() + ingredient_2() + ingredient_3();
+
+// nothing special
+PRICES[COCKTAIL_ONE] = 103;
+
+// bitte ein bit
+PRICES[BEER_ONE] = 74;
+
+// corona 2019
+PRICES[BEER_TWO] = 110;
--- assignment_one/test/bar.spec.ts
+++ assignment_one/test/bar.spec.ts
@@ -1,6 +1,25 @@
-import { BEER_ONE, BEERTWO, CocktailOne, cocktail2, special_cocktail, calculateCost } from "../src/bar";
+import { BEER_ONE, BEER_TWO,
+  COCKTAIL_ONE, COCKTAIL_TWO, COCKTAIL_SPECIAL,
+  calculateCost, isBeer, isCocktail } from "../src/bar";
 import { expect, assert, should } from 'chai';
 import 'mocha';
+
+describe('Test correct initialization', () => {
+    it('Correctly detects beer', () => {
+        expect(isBeer(COCKTAIL_ONE)).to.be.false;
+        expect(isBeer(BEER_ONE)).to.be.true;
+	expect(isBeer(BEER_TWO)).to.be.true;
+	expect(isBeer('')).to.be.false;
+	expect(isBeer('CoRONa')).to.be.true;
+    });
+    it('Correctly detects cocktails', () => {
+        expect(isCocktail(BEER_ONE)).to.be.false;
+        expect(isCocktail(COCKTAIL_ONE)).to.be.true;
+	expect(isCocktail(COCKTAIL_TWO)).to.be.true;
+	expect(isCocktail(COCKTAIL_SPECIAL)).to.be.true;
+	expect(isCocktail()).to.be.false;
+    });
+});
 
 describe('Ordering drinks gives the expected price', () => {
     it('Correctly calculates 74kr for a Heineken', () => {
@@ -8,16 +27,20 @@ describe('Ordering drinks gives the expected price', () => {
         expect(price).to.equal(74);
     });
     it('Correctly calculates 103kr for a Mojito cocktail', () => {
-        const price = calculateCost(CocktailOne, false, 1);
+        const price = calculateCost(COCKTAIL_ONE, false, 1);
         expect(price).to.equal(103);
     });
     it('Correctly calculates 115kr for a Margerita cocktail', () => {
-        const price = calculateCost(cocktail2, false, 1);
+        const price = calculateCost(COCKTAIL_TWO, false, 1);
         expect(price).to.equal(115);
     });
     it('Correctly calculates 127.5 kr for a special bacardi cocktail', () => {
-        const price = calculateCost(special_cocktail, false, '1');
+        const price = calculateCost(COCKTAIL_SPECIAL, false, '1');
         expect(price).to.equal(128);
+    });
+    it('Correctly calculates 74kr for a Heineken with default params', () => {
+        const price = calculateCost(BEER_ONE);
+        expect(price).to.equal(74);
     });
 });
 
@@ -25,7 +48,7 @@ describe('Student discounts are correctly handled', () => {
     it('Correctly gives 10% discount for beers', () => {
         const price = calculateCost(BEER_ONE, true, 1);
         expect(price).to.equal(67);
-        const price2 = calculateCost(BEERTWO, true, 1);
+        const price2 = calculateCost(BEER_TWO, true, 1);
         expect(price2).to.equal(99);
     });
     it('Also gives discount for multiple beers', () => {
@@ -33,11 +56,11 @@ describe('Student discounts are correctly handled', () => {
         expect(price).to.equal(200);
     });
     it('Does not allow discounts for cocktails', () => {
-        const price = calculateCost(CocktailOne, true, 1);
+        const price = calculateCost(COCKTAIL_ONE, true, 1);
         expect(price).to.equal(103);
-        const price1 = calculateCost(cocktail2, true, 1);
+        const price1 = calculateCost(COCKTAIL_TWO, true, 1);
         expect(price1).to.equal(115);
-        const price2 = calculateCost(special_cocktail, true, 1);
+        const price2 = calculateCost(COCKTAIL_SPECIAL, true, 1);
         expect(price2).to.equal(128);
     });
 });
@@ -48,9 +71,16 @@ describe('Error handling is correct', () => {
         assert.throws(() => calculateCost(COCKTAIL_THREE, false, 1), 'Drink does not exist');
     });
     it('Will NOT allow ordering more than two cocktails', () => {
-        assert.throws(() => calculateCost(CocktailOne, false, 3), 'Not allowed to order more than 2 cocktails');
+        assert.throws(() => calculateCost(COCKTAIL_ONE, false, 3), 'Not allowed to order more than 2 cocktails');
     });
     it('Will allow ordering more than 2 beers', () => {
         assert.doesNotThrow(() => calculateCost(BEER_ONE, false, 3))
     });
+    it('Raise exception on zero count of drinks', () => {
+        assert.throws(() => calculateCost(COCKTAIL_SPECIAL, false, 0), 'Invalid amount of drinks ordered' );
+    });
+    it('Raise exception when all params are default', () => {
+        assert.throws(() => calculateCost(), 'Drink does not exist' );
+    });
 });
+
